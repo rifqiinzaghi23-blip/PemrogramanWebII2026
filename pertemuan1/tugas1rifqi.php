d040e605193df09bc29762041a7e9ea28b3885bf
rifqiinzaghi23-blip <rifqiinzaghi23@gmail.com>
2026-04-08 11:59:24 +0700

    Create tugas1rifqi.php

---
+++ pertemuan1/tugas1rifqi.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>mahasiswa</title>
+</head>
+<body>
+
+<h2>Data Nilai Mahasiswa</h2>
+
+<?php
+class Mahasiswa {
+    // Property
+    public $nama;
+    public $nim;
+    public $jurusan;
+
+    // Method
+    public function tampilData() {
+        echo "Nama    : " . $this->nama . "<br>";
+        echo "NIM     : " . $this->nim . "<br>";
+        echo "Jurusan : " . $this->jurusan . "<br>";
+    }
+}
+
+// Membuat object
+$mhs1 = new Mahasiswa();
+
+// Mengisi data
+$mhs1->nama = "Andi";
+$mhs1->nim = "12345";
+$mhs1->jurusan = "Teknik Informatika";
+
+// Menampilkan data
+$mhs1->tampilData();
+?>
